3fe1feb265c5388c308a532be36c50ff1137ee89
rapii03 <rafirmdhn32@gmail.com>
2024-05-22 21:18:11 +0700

    fungsi edit dosen

--- app/Http/Controllers/LectureController.php
+++ app/Http/Controllers/LectureController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LectureAddRequest;
+use App\Http\Requests\LectureEditRequest;
 use Illuminate\Http\Request;
 use App\Models\Lecture;
 
@@ -11,5 +12,23 @@ class LectureController extends Controller
     public function tambah(LectureAddRequest $request)
     {
         return Lecture::create($request->safe()->all());
+    }
+
+    public function ubah(LectureEditRequest $request, Lecture $lecture)
+    {
+        $newEmail = $request['email'];
+
+        if ($newEmail !== $lecture->email) {
+            $exists = Lecture::whereKeyNot($lecture->id)
+                ->where('email', $newEmail)
+                ->first();
+
+            if ($exists) {
+                return back()
+                    ->withErrors(['email' => '']);
+            }
+        }
+
+        return $lecture->update($request->safe()->all());
     }
 }
---
+++ app/Http/Requests/LectureEditRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LectureEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'bail|required|string|max:255|email',
+            'address' => 'bail|required|string|max:750',
+            'phone' => 'bail|required|string|max:255',
+            'name' => 'bail|required|string|max:255',
+            'nidn' => 'bail|required|string|max:255',
+            'nip' => 'bail|required|string|max:255',
+            'kk' => 'bail|required|string|max:255',
+        ];
+    }
+}
