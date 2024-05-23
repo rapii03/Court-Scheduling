283ef46b2f1edbc056e6620eaa0278e618a44795
rapii03 <rafirmdhn32@gmail.com>
2024-05-23 17:13:38 +0700

    menambahkan model dan migrasi data dosen

---
+++ app/Models/LectureData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class LectureData extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'phone_number',
+        'address',
+        'image',
+        'nidn',
+        'nip',
+        'kk',
+
+        'user_id',
+    ];
+}
---
+++ database/migrations/2024_05_23_100541_create_lecture_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lecture_data', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->text('image')->nullable();
+            $table->string('nidn');
+            $table->string('nip');
+            $table->string('kk');
+
+            $table->timestamps();
+
+            $table->foreignUuid('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lecture_data');
+    }
+};
