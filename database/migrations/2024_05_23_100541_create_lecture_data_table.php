4ae500829942d312ea6bec919e68435a3bae8503
rapii03 <rafirmdhn32@gmail.com>
2024-05-23 17:15:07 +0700

    kolom user_id tabel data dosen menjadi unik

--- database/migrations/2024_05_23_100541_create_lecture_data_table.php
+++ database/migrations/2024_05_23_100541_create_lecture_data_table.php
@@ -22,7 +22,7 @@ return new class extends Migration {
 
             $table->timestamps();
 
-            $table->foreignUuid('user_id')->constrained('users');
+            $table->foreignUuid('user_id')->unique()->constrained('users');
         });
     }
 
