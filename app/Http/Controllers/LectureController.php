f39428a315549e413cbefbbbf1f221784c532d72
rapii03 <rafirmdhn32@gmail.com>
2024-05-23 18:27:46 +0700

    fungsi edit dosen

--- app/Http/Controllers/LectureController.php
+++ app/Http/Controllers/LectureController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LectureAddReq;
+use App\Http\Requests\LectureEditReq;
 use Illuminate\Http\Request;
 use App\Models\LectureData;
 use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class LectureController extends Controller
 {
@@ -25,6 +27,33 @@ class LectureController extends Controller
 
         $lecture->user()->associate($account);
         $lecture->save();
+
+        return true;
+    }
+
+    public function edit(LectureEditReq $request, User $account)
+    {
+        $newEmail = $request['email'];
+        if ($newEmail !== $account->email) {
+            $exists = User::whereKeyNot($account->id)
+                ->where('email', $newEmail)
+                ->first();
+
+            if ($exists) {
+                return back()->withErrors(['email', 'email already exists']);
+            }
+        }
+        $lecture = LectureData::whereBelongsTo($account)->firstOrFail();
+
+        $account->update($request->safe()->only('email', 'name'));
+        $lecture->update($request->safe()->except('email', 'image', 'name'));
+
+        if ($request->hasFile('image')) {
+            Storage::delete($lecture->image);
+
+            $lecture->image = $request->file('image')->store('lectureImages');
+            $lecture->save();
+        }
 
         return true;
     }
