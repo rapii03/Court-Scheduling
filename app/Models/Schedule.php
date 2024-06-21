003b26cd4207479cb67c9642374083323b82a38b
rapii03 <rafirmdhn32@gmail.com>
2024-06-21 21:43:12 +0700

    menambahkan model dan migrasi jadwal

---
+++ app/Models/Schedule.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'date',
+        'seminar_id',
+        'time_id',
+    ];
+}
---
+++ database/migrations/2024_06_21_143004_create_schedules_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->date('date');
+
+            $table->foreignUuid('seminar_id')->constrained('seminars');
+            $table->foreignUuid('time_id')->constrained('time_lists');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
